c68e0ffc347395db4b0bc24a86be041ff91e3669
joegsuero <josedaniel.garcia@datazucar.cu>
2025-04-07 21:56:13 -0400

    Fin in styles and pagination of filtered

--- src/pages/entries/components/PostList.tsx
+++ src/pages/entries/components/PostList.tsx
@@ -21,9 +21,16 @@ function PostList({
   const [currentPage, setCurrentPage] = useState(1);
   const [filter, setFilter] = useState("");
 
-  const totalPages = Math.ceil(posts.length / ITEMS_PER_PAGE);
+  const filteredPosts = posts.filter(
+    (post) =>
+      post.content.toLowerCase().includes(filter) ||
+      post.title.toLowerCase().includes(filter) ||
+      post.tags.toLowerCase().includes(filter)
+  );
 
-  const currentPosts = posts.slice(
+  const totalPages = Math.ceil(filteredPosts.length / ITEMS_PER_PAGE);
+
+  const currentPosts = filteredPosts.slice(
     (currentPage - 1) * ITEMS_PER_PAGE,
     currentPage * ITEMS_PER_PAGE
   );
@@ -70,42 +77,35 @@ function PostList({
         />
       </div>
       <ul className="post-list">
-        {currentPosts
-          .filter(
-            (post) =>
-              post.content.toLowerCase().includes(filter) ||
-              post.title.toLowerCase().includes(filter) ||
-              post.tags.toLowerCase().includes(filter)
-          )
-          .map((post, index) => (
-            <li
-              key={index}
-              className="post-item"
-              onClick={() =>
-                onEditPost((currentPage - 1) * ITEMS_PER_PAGE + index)
-              }
+        {currentPosts.map((post, index) => (
+          <li
+            key={index}
+            className="post-item"
+            onClick={() =>
+              onEditPost((currentPage - 1) * ITEMS_PER_PAGE + index)
+            }
+          >
+            <h2>{post.title}</h2>
+            <p className="post-content" title={post.content}>
+              {truncateText(post.content, MAX_CONTENT_LENGTH)}
+            </p>
+            <div className="tags">
+              {post.tags.length > 0 &&
+                post.tags.split(",").map((tag: string, idx: number) => (
+                  <span key={idx} className="tag">
+                    {tag}
+                  </span>
+                ))}
+            </div>
+            <button
+              type="button"
+              className="delete-button"
+              onClick={(event) => handleDelete(event, post.id)}
             >
-              <h2>{post.title}</h2>
-              <p className="post-content" title={post.content}>
-                {truncateText(post.content, MAX_CONTENT_LENGTH)}
-              </p>
-              <div className="tags">
-                {post.tags.length > 0 &&
-                  post.tags.split(",").map((tag: string, idx: number) => (
-                    <span key={idx} className="tag">
-                      {tag}
-                    </span>
-                  ))}
-              </div>
-              <button
-                type="button"
-                className="delete-button"
-                onClick={(event) => handleDelete(event, post.id)}
-              >
-                <FaTrash />
-              </button>
-            </li>
-          ))}
+              <FaTrash />
+            </button>
+          </li>
+        ))}
       </ul>
 
       {/* Paginación */}
@@ -120,11 +120,11 @@ function PostList({
           </button>
 
           <span className="page-indicator">
-            Page {currentPage} of {totalPages}
+            Page {totalPages == 0 ? 0 : currentPage} of {totalPages}
           </span>
 
           <button
-            disabled={currentPage === totalPages}
+            disabled={totalPages == 0 || currentPage === totalPages}
             onClick={() => setCurrentPage(currentPage + 1)}
             className="pagination-button"
           >
